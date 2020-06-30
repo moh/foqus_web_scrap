e8c30f386ad01321eb43b860cf402e0a7b856f95
mhd <saidm>
2020-06-30 16:23:08 +0200

    UPDATE on finding the images related to products in a general way.

--- foqusBot/foqusBot/spiders/general.py
+++ foqusBot/foqusBot/spiders/general.py
@@ -12,6 +12,9 @@ from foqusBot.common_words import *
 # MIN_RATIO is the min ratio where the page is still identified as home or product page
 MIN_RATIO = 0.3
 
+# the number of pages to learn from to extract wich method is used frequently, and wich class is detected in title div method ( for images)
+LEARN_IMG_NB_PAGE = 20
+
 class GeneralSpider(scrapy.Spider):
     name = 'generalFoqus'
 
@@ -23,12 +26,24 @@ class GeneralSpider(scrapy.Spider):
 
     shared_product_info : dict that have keys the base of the website, and value the shared part of product
                         informations (usually it is related to general information about the website)
+
+    methods_stat : dict that have keys the base of the website, and value a list that represent the number of time
+    each method (of extracting images) is used on the website. ( first number represents the first method ( class
+    name method), second number represents the second method ( title div method ))
+
+    titleImg_method_classes : dict that have keys the base of the website, and value a dictionary that represents the
+    class found of the div as a key and a list that contains the number of repetition of that class, and the
+    score associated to it (score  = number of img found) as a value
+    
+    order : the order of excuting the request in parse method
     '''
     def __init__(self, *a, **kw):
         super(GeneralSpider, self).__init__(*a, **kw)
         self.url_home_products = dict()
         self.url_classes = dict()
         self.shared_product_info = dict()
+        self.methods_stat = dict()
+        self.titleImg_method_classes = dict()
         self.order = 0
         
 
@@ -87,7 +102,9 @@ class GeneralSpider(scrapy.Spider):
         home_classes = homePageClasses - final_classes
 
         self.url_classes[self.getUrlBase(response.url)] = [home_classes, product_classes]
-        
+
+        # initiate the method statistics to zeros
+        self.methods_stat[self.getUrlBase(response.url)] = [0, 0]
         # call the home page to analyse links
         yield Request(self.url_home_products[self.getUrlBase(response.url)][0], dont_filter = True)
         
@@ -175,26 +192,40 @@ class GeneralSpider(scrapy.Spider):
 
     '''
     def getProductInfo(self, response, product_shared):
+        url_base = self.getUrlBase(response.url)
+        method = ""
+        nb_pages, ratio_first_method = sum(self.methods_stat[url_base]), 0
         product_classes = [x for x in product_shared if self.shareWithList(x, product_identifiers)]
-        print("product classes =  ", product_classes)
         product_classes = [x for x in product_classes if len(response.xpath('//*[contains(@class, "'+x+'")]')) == 1]
 
         image_classes = [x for x in product_classes if self.shareWithList(x, images_identifiers)]
-        infos_classes = [x for x in product_classes if self.shareWithList(x, infos_identifiers)]
+        # infos_classes = [x for x in product_classes if self.shareWithList(x, infos_identifiers)]
         title_classes = [x for x in product_classes if self.shareWithList(x, title_identifiers)]
         
         images = self.getImgsFromClasses(response ,image_classes) # get the images
         titles = self.getTitle(response, title_classes)
-        informations = self.getInfosFromClasses(response, infos_classes)
-        all_informations = self.getAllInfosFromClasses(response, infos_classes)
+        # informations = self.getInfosFromClasses(response, infos_classes)
+        # all_informations = self.getAllInfosFromClasses(response, infos_classes)
 
-        if len(images) <= 2:
+        if nb_pages != 0:
+            ratio_first_method = self.methods_stat[url_base][0] / nb_pages
+        
+        # if ratio_first_method is less then half and the pages that we have tested are more then 10, then
+        # we always do the second method
+        if (len(images) <= 2 and nb_pages < LEARN_IMG_NB_PAGE) or (ratio_first_method < 0.5 and nb_pages >= LEARN_IMG_NB_PAGE):
             images = self.getImgFromTitleDiv(response)
-        
-        return {"url" : response.url,"images" : images, "titles" : titles, "informations": informations, "all_informations":all_informations} #, "price" : price}
-        
-        #return {"url" : response.url, "product_classes" : product_classes, "product_shared" : product_shared , "image_classes" : gallery_classes, "infos_classes" : infos_classes}
+            self.methods_stat[url_base][1] += 1 # update number related to second method.
+            method = "2"
+        else:
+            self.methods_stat[url_base][0] += 1 # update number related to first method.
+            method = "1"
+            
+        #print("\n Method statisticss !! :::: ", self.methods_stat[url_base])
+        #return {"url" : response.url,"images" : images, "titles" : titles, "informations": informations,
+        #       "all_informations":all_informations, "method":method, "stat":self.titleImg_method_classes} #, "price" : price}
 
+        return {"url" : response.url,"images" : images, "titles" : titles, "method":method, "stat":self.titleImg_method_classes}
+        
     '''
 
     Return the images src URL that are descendant of a div that have the classes in the list.
@@ -285,12 +316,48 @@ class GeneralSpider(scrapy.Spider):
     first method based on class names didn't work.
     it looks for the h1 tag that contains the title, then search for the images by iterating the
     ancestor of the title, it relys on the number of images found. ( more then 1 )
+    it also store the class of the child div found, and then if we have a repeated class over many page, we will
+    suppose that in all the other page this div contain the searched images.
     
     '''
     def getImgFromTitleDiv(self, response):
+        url_base = self.getUrlBase(response.url)
+
+        try:
+            stat_dict = self.titleImg_method_classes[url_base]
+            # select the class that have maximum number of pages
+            max_classe_ByNb = max(stat_dict, key = lambda x : stat_dict[x][0])
+
+            # select the class that have maximum number of score ( nb of images )
+            max_classe_ByScore = max(stat_dict, key = lambda x : stat_dict[x][1])
+            
+            nb_pages = sum([x[0] for x in stat_dict.values()]) # total number of pages
+            total_scores = sum([x[1] for x in stat_dict.values()]) # total scores
+            
+            # if more them LEARN_IMG_NB_PAGE have been scraped then we try to follow old pages
+            if nb_pages >= LEARN_IMG_NB_PAGE:
+                selected_class = ""
+                # if the class with max score have a score more then 0.5 of total score, and has been used in more then 0.4 of classes then we use it
+                if (stat_dict[max_classe_ByScore][0]/nb_pages >= 0.4 and stat_dict[max_classe_ByScore][1]/total_scores >= 0.5):
+                    selected_class = max_classe_ByScore
+                    
+                # if the most used class have a ration of more then 0.5 then we return images inside the div of that class
+                elif stat_dict[max_classe_ByNb][0]/nb_pages >= 0.5:
+                    selected_class = max_classe_ByNb
+                if selected_class != "":
+                    imgs = self.getImgsByClassedDiv(response, selected_class)
+                    if len(imgs) > 0:
+                        self.titleImg_method_classes[url_base][selected_class][0] += 1
+                        self.titleImg_method_classes[url_base][selected_class][1] += len(imgs) # update the score 
+                        return imgs
+                    
+        # in case url_base is not in dict then initialise it
+        except KeyError:
+            self.titleImg_method_classes[url_base] = dict()
+            
         title_div = response.css("h1") # get h1 div
         if len(title_div) == 0: return [] # if no h1 tag found return empty list
-        title_div = title_div[0] # select the last h1 div.
+        title_div = title_div[0] # select the first h1 div ( it should be the title )
         title_div_parents = title_div.xpath("ancestor::*")
         div_parent, div_child = None, None
         for div in title_div_parents[::-1]:
@@ -299,6 +366,7 @@ class GeneralSpider(scrapy.Spider):
                 break
         # if no parent have more then 1 image we return []
         if div_parent == None: return []
+        # search for the first descendant of div_parent that satisfies the condition
         div_childrens = div_parent.xpath("descendant::div")
         for div in div_childrens:
             nb_img = len(set(div.xpath("descendant::img/@src").getall()))
@@ -306,8 +374,26 @@ class GeneralSpider(scrapy.Spider):
                 div_child = div
                 break
         if div_child == None: return []
-        images = set(div_child.xpath("descendant::img/@src").getall())
+        images = set(div_child.xpath("descendant::img/@src").getall()) # get the src of images inside the div_child tag
+
+        div_child_class = div_child.xpath("@class").get()
+        try:
+            self.titleImg_method_classes[url_base][div_child_class][0] += 1
+            self.titleImg_method_classes[url_base][div_child_class][1] += len(images) # update the score 
+        except: # div_child_class is not a key 
+            self.titleImg_method_classes[url_base][div_child_class] = [1, len(images)] # initiate the number of pages to 1 and initiate the score.
+        
         return [response.urljoin(x) for x in images]
+
+    '''
+    get the images inside the div elements that have class name = classe
+    '''
+    def getImgsByClassedDiv(self, response, classe):
+        div_imgs = response.xpath("//div[@class = '" + classe + "']/descendant::img/@src").getall()
+        return list({response.urljoin(x) for x in div_imgs})
+        
+        
+
         
     '''
 
